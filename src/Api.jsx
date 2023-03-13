8a5087eb9966d680914ad22a21aa9d5ccd9894ba
leandro alfonso <leandro.meira.alfonso@hotmail.com>
2023-03-13 01:07:05 -0300

    arrumado api

--- src/Api.jsx
+++ src/Api.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 export default axios.create({
     
-    baseURL : 'https://confused-cow-scarf.cyclic.app//'
+    baseURL : 'https://confused-cow-scarf.cyclic.app/'
 
 });
 
